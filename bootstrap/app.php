067306df83dcae3094a8403164206ea591395fec
Sandi Mulyadi <sandimvlyadi@gmail.com>
2026-04-02 00:16:08 +0700

    trustProxies

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
+        $middleware->trustProxies('*');
 
         $middleware->web(append: [
             HandleAppearance::class,
